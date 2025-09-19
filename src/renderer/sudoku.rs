d4a556f435007658577f612595d23ecb95ed8d26
Jan-Henrik Bruhn <jan-henrik.bruhn@offis.de>
2025-09-19 23:05:42 +0200

    Rework sudoku rendering

--- src/renderer/sudoku.rs
+++ src/renderer/sudoku.rs
@@ -2,10 +2,8 @@ use crate::printer::Command;
 use escpos::utils::JustifyMode;
 use rustoku_lib::generate_board;
 
-pub async fn make_sudoku() -> Vec<Command> {
-    let sudoku = generate_board(40).expect("sudokus should always be solvable.");
-    let mut commands = vec![Command::ResetSize, Command::Justify(JustifyMode::CENTER)];
-
+fn render_sudoku(board: rustoku_lib::core::Board) -> String {
+    let mut s = String::with_capacity(3 * 9 * 9 * 3);
     const CHARS: [[&str; 4]; 4] = [
         ["┌", "┬", "╥", "┐"], // top
         ["├", "┼", "╫", "┤"], // thin sep
@@ -18,61 +16,68 @@ pub async fn make_sudoku() -> Vec<Command> {
         if row == 0 || row == 9 {
             let chars = &CHARS[if row == 0 { 0 } else { 3 }];
 
-            commands.push(Command::Write(String::from(chars[0])));
+            s.push_str(chars[0]);
             for i in 0..9 {
-                commands.push(Command::Write(String::from("───")));
+                s.push_str("───");
                 if i < 8 {
-                    commands.push(Command::Write(String::from(
-                        chars[if i % 3 == 2 { 2 } else { 1 }],
-                    )));
+                    s.push_str(chars[if i % 3 == 2 { 2 } else { 1 }]);
                 }
             }
-            commands.push(Command::Write(String::from(chars[3]) + "\n"));
+            s.push_str(chars[3]);
+            s.push_str("\n");
         }
 
         if row < 9 {
             // Data row
-            commands.push(Command::Write(String::from("|")));
+            s.push_str("|");
             for col in 0..9 {
-                let n = sudoku.get(row, col);
-                commands.push(Command::Write(format!(
+                let n = board.get(row, col);
+                s.push_str(&format!(
                     " {} ",
                     if n > 0 {
                         n.to_string()
                     } else {
                         " ".to_string()
                     }
-                )));
-                commands.push(Command::Write(String::from(if col % 3 == 2 && col < 8 {
+                ));
+                s.push_str(if col % 3 == 2 && col < 8 {
                     "║"
                 } else {
                     "│"
-                })));
+                });
             }
-            commands.push(Command::Write(String::from("\n")));
+            s.push_str("\n");
 
             // Separator
             if row < 8 {
                 let chars = &CHARS[if row % 3 == 2 { 2 } else { 1 }];
-                commands.push(Command::Write(String::from(chars[0])));
+                s.push_str(chars[0]);
                 for i in 0..9 {
-                    commands.push(Command::Write(String::from(if row % 3 == 2 {
+                    s.push_str(if row % 3 == 2 {
                         "═══"
                     } else {
                         "───"
-                    })));
+                    });
                     if i < 8 {
-                        commands.push(Command::Write(String::from(
-                            chars[if i % 3 == 2 { 2 } else { 1 }],
-                        )));
+                        s.push_str(chars[if i % 3 == 2 { 2 } else { 1 }]);
                     }
                 }
-                commands.push(Command::Write(String::from(chars[3]) + "\n"));
+                s.push_str(chars[3]);
+                s.push_str("\n");
             }
         }
     }
-    commands.push(Command::Justify(JustifyMode::LEFT));
-    commands.push(Command::ResetSize);
 
-    commands
+    s
+}
+
+pub async fn make_sudoku() -> Vec<Command> {
+    let sudoku = generate_board(40).expect("sudokus should always be solvable.");
+    vec![
+        Command::ResetSize,
+        Command::Justify(JustifyMode::CENTER),
+        Command::Write(render_sudoku(sudoku)),
+        Command::Justify(JustifyMode::LEFT),
+        Command::ResetSize,
+    ]
 }
